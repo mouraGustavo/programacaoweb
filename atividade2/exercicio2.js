c7938e46407089087461a4b8e5048f7070fa0e54
Gustavo B. Moura <134016288+mouraGustavo@users.noreply.github.com>
2023-08-20 15:48:52 -0300

    exercicios2, matriz transposta

--- atividade2/exercicio2.js
+++ atividade2/exercicio2.js
@@ -1,4 +1,4 @@
-var mat1 = [
+/* var mat1 = [
     [1,2], [3,4], [5,6],
     [1,2], [3,4], [5,6],
     [1,2], [3,4], [5,6]
@@ -13,4 +13,28 @@ for(i=0; i<mat1.length; i++){
     for(j=0;j<(mat1.length);j++){
         console.log(mat1[i][j]);
     }
+} */
+var mat1 = [
+    [1, 2], [3, 4],
+    [1, 2], [3, 4],
+    [1, 2], [3, 4],
+];
+
+console.log('Matriz:');
+mat1.forEach(row => {
+    console.log(row.join(' '));
+});
+
+console.log('Matriz transposta:');
+const matTransposed = [];
+
+for (let i = 0; i < mat1[0].length; i++) {
+    matTransposed.push([]);
+    for (let j = 0; j < mat1.length; j++) {
+        matTransposed[i].push(mat1[j][i]);
+    }
 }
+
+matTransposed.forEach(row => {
+    console.log(row.join(' '));
+});
