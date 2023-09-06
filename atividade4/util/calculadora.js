719e4579fb6b2f5d140838a5c9ca60f14316a052
GUSTAVO BORGES MOURA <moura.gborges@gmail.com>
2023-09-06 09:56:07 -0300

    Ajustes

--- atividade4/util/calculadora.js
+++ atividade4/util/calculadora.js
@@ -1,15 +1,15 @@
 module.exports = {
     somar: function(a, b){
-        return parseInt(a) + parseInt(parseInt(b));
+        return parseInt(a) + parseInt(b);
     }, 
     subtrair: function (a, b){
-        return parseInt(a)-parseInt(b);
+        return parseInt(a-b);
     }, 
     multiplicar: function(a, b){
         return parseInt(a)*parseInt(b);
     },
     dividir: function(a, b){
-        return parseInt(a)/parseInt(parseInt(b));
+        return parseInt(a)/parseInt(b);
     }
     // dividir: (a, b) => a/b
     // multiplicar: (a, b) => a*b
